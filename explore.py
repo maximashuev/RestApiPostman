2ea4d21519adef231796f78ed582924b71ac29cb
maxim ashuev <maximashuev@gmail.com>
2020-10-20 08:30:51 -0500

    Updated explore.py on mac

--- explore.py
+++ explore.py
@@ -3,4 +3,13 @@ from twilio.rest import Client
 client = Client("ACcbb9a272aed2141702026e753988853b", "9d8b943148e5ae6a679b46a4243d76ca")
 
 for msg in client.messages.list():
-    print(msg.body)
+    print(f"Deleting {msg.body}")
+    msg.delete()
+
+
+# msg=client.messages.create(
+#     from_="+12058285281",
+#     to="+19795714727",
+#     body="Hello from Python"
+# )
+# print(f"Created a new message: {msg.sid}")
