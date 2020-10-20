927b13f6deaab832b2d9f2a70de9549aee7f8d58
maxim ashuev <maximashuev@gmail.com>
2020-10-20 08:32:59 -0500

    Updated explore.py on mac

--- explore.py
+++ explore.py
@@ -3,6 +3,9 @@ from twilio.rest import Client
 client = Client("ACcbb9a272aed2141702026e753988853b", "9d8b943148e5ae6a679b46a4243d76ca")
 
 for msg in client.messages.list():
+    print(msg.body)
+
+for msg in client.messages.list():
     print(f"Deleting {msg.body}")
     msg.delete()
 
